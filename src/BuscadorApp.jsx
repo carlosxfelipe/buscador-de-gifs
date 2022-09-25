e2c54211a64d157a3e8b3af7675b75d162f07286
Carlos Felipe Araújo <carlosxfelipe@gmail.com>
2022-09-25 14:43:10 -0300

    first commit

---
+++ src/BuscadorApp.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const BuscadorApp = () => {
+  const [categories, setCategories] = useState([]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>Buscador de Gifs</h1>
+      <AddCategory onNewCategory={onAddCategory} />
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async (category) => {
+  const api = "6UzbwP9zb8v5a6n0koIChEGJAJb1s2w0";
+  const url = `api.giphy.com/v1/gifs/search?api_key=${api}q=${category}&limit=10`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+
+  return gifs;
+};
