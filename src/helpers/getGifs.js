deb5e4ff47c8e6e91cb9f1b273fc2b57f690ab85
Carlos Felipe Araújo <carlosxfelipe@gmail.com>
2022-09-26 10:11:17 -0300

    api

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
   const api = "6UzbwP9zb8v5a6n0koIChEGJAJb1s2w0";
-  const url = `api.giphy.com/v1/gifs/search?api_key=${api}q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${api}&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
